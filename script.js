840648e4db656c4642a140259077adc2d5eb3e41
Roman <fynn.eyad.00@bk.ru>
2023-01-03 14:23:05 +0300

    end

--- script.js
+++ script.js
@@ -15,3 +15,6 @@ const  a = prompt('Один из последних просмотренных �
        d = prompt('На сколько оцените его?','');
 
 personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
